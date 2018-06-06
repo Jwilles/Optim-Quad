7fcb9ba04be9c3f903e706775cc6c03362cdd77b
John Willes <johnwilles@gmail.com>
2018-06-05 22:57:42 -0400

    Reformat objective function

--- OptimMain.m
+++ OptimMain.m
@@ -1,6 +1,5 @@
 clc
 
-
 A=[];
 b=[];
 Aeq=[];
@@ -13,17 +12,17 @@ load D.mat
 
 fmin=0;
 
-
 for i=1:10
-x0=D(:,i);
-options = optimoptions('fmincon','Algorithm','interior-point','Display', 'iter', 'MaxFunEvals', 3000, 'MaxIter', 1000);%'PlotFcns', { @optimplotfval })%@optimplotx @optimplotconstrviolation});% 'TolFun', 1e-3, 'TolX', 1e-3
-[x,fval,exitflag,output]=fmincon(@ObjectiveFunction,x0,A,b,Aeq,beq,lb,ub,@nonlcon,options)
+  x0=D(:,i);
+  options = optimoptions('fmincon','Algorithm','interior-point','Display', 'iter', 'MaxFunEvals', 3000, 'MaxIter', 1000);%'PlotFcns', { @optimplotfval })%@optimplotx @optimplotconstrviolation});% 'TolFun', 1e-3, 'TolX', 1e-3
+  [x,fval,exitflag,output]=fmincon(@ObjectiveFunction,x0,A,b,Aeq,beq,lb,ub,@nonlcon,options)
+  
+  if fval<fmin
+      fmin=fval;
+  end
 
-if fval<fmin
-    fmin=fval;
 end
 
-end
 load PropModel.mat
 load MotorModel.mat
 load BatModel.mat
@@ -34,5 +33,3 @@ BatData=predictor(x(2),BatModel)
 MotorData=predictor(x(4),MotorModel)
 
 nonlcon(x)
-
-
